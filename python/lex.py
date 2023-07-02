a4f91bad359aad3b2e0242e3f9d2500d461553d9
NuckenMcFuggets <cameron.ian.ronald.ord@Outlook.com>
2023-07-02 17:34:30 -0600

    lex and parse

--- python/lex.py
+++ python/lex.py
@@ -36,33 +36,40 @@ class Parser:
 
     def factor(self):
         
-        token = self.current_token()
-        if token[0] == 'NUMBER':
-            self.consume_token()
-            print(token)
-            return float(token[1])
-        elif token[0] == 'IDENTIFIER':
-            self.consume_token()
-            print(token)
-            return token[1]
-        elif token[0] == 'OPERATOR':
-            self.consume_token()
-            print(token)
-            return token[1]
-        elif token[0] == 'PUNCTUATION':
-            self.consume_token()
-            print(token)
-            return token[1]
-        elif token[0] == 'STRING':
-            self.consume_token()
-            print(token)
-            return token[1]
-        elif token[0] =='WHITESPACE':
-            self.consume_token()
-            print(token)
-            return token[1]
-        else:
-            raise Exception('Invalid syntax')
+        parsed_tokens = []
+
+        while self.current_token_index < len(self.tokens):
+            token = self.current_token()
+
+            if token[0] == 'NUMBER':
+                self.consume_token()
+                print(token)
+                parsed_tokens.append(token[1])
+            elif token[0] == 'IDENTIFIER':
+                self.consume_token()
+                print(token)
+                parsed_tokens.append(token[1])
+            elif token[0] == 'OPERATOR':
+                self.consume_token()
+                print(token)
+                parsed_tokens.append(token[1])
+            elif token[0] == 'PUNCTUATION':
+                self.consume_token()
+                print(token)
+                parsed_tokens.append(token[1])
+            elif token[0] == 'STRING':
+                self.consume_token()
+                print(token)
+                parsed_tokens.append(token[1])
+            elif token[0] == 'WHITESPACE':
+                self.consume_token()
+                print(token)
+                parsed_tokens.append(token[1])
+            else:
+                raise Exception('Invalid syntax')
+
+        return parsed_tokens
+
 
     def current_token(self):
         return self.tokens[self.current_token_index]
